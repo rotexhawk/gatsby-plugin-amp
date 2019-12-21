81afd1da3f4f195892276b22ffd4f9b652e4549d
Yasin Yaqoobi <yasinyaqoobi@gmail.com>
2019-12-21 10:17:42 -0500

    remove exclude/include paths

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,9 +1,7 @@
 import React, { Fragment } from "react";
 import { renderToString } from "react-dom/server";
-import { Minimatch } from "minimatch";
 import flattenDeep from "lodash.flattendeep";
 const JSDOM = eval('require("jsdom")').JSDOM;
-const minimatch = require("minimatch");
 
 const ampBoilerplate = `body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}`;
 const ampNoscriptBoilerplate = `body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}`;
@@ -23,12 +21,8 @@ export const onPreRenderHTML = (
   },
   {
     analytics,
-    canonicalBaseUrl,
     components = [],
-    includedPaths = [],
-    excludedPaths = [],
     pathIdentifier = "/amp/",
-    relAmpHtmlPattern = "{{canonicalBaseUrl}}{{pathname}}{{pathIdentifier}}"
   }
 ) => {
   const headComponents = flattenDeep(getHeadComponents());
